6e7a1c7e39b536839da874dc4b1bdc695e0429b2
Satyam250304 <228w1a05i5@vrsec.ac.in>
2025-10-16 01:08:46 +0530

    about section animated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion.js";
 import ServiceCard from "./canvas/SpotlightCard";
+import { SectionWrapper } from "../hoc";
 
 const About = () => {
   return (
@@ -37,4 +38,5 @@ const About = () => {
   );
 };
 
-export default About;
+//higher order component wrapping into a section wrapper: component, id 
+export default SectionWrapper(About, "about");
